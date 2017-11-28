19190ceef80496d2902c2b7e5135e0725e7c93a8
Michael Kuang <mkuang@ciena.com>
2017-11-28 11:47:08 -0500

    sortofsorting

---
+++ kattis/src/tray.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class tray {
+
+    public static void main(String[]args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = in.readLine().split(" ");
+        int m = Integer.parseInt(input[0]);
+        int n = Integer.parseInt(input[1]);
+        //calculate the number of orientations a 1x2 foot tray can be placed
+
+
+    }
+}
