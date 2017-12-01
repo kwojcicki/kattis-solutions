c2e5a5b9f3554b1048ae22db8b70a784027bbdeb
kwojcicki <kwojcicki@sympatico.ca>
2017-12-01 14:15:37 -0500

    added candy division

---
+++ kattis/src/candydivision.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Iterator;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class candydivision {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		long n = Long.parseLong(in.readLine());
+		SortedSet<Integer> list = new TreeSet<Integer>();
+		for(int i = 2; i <= Math.ceil(Math.sqrt((double)n)); i++){
+			if(n % i == 0){
+				list.add(i);
+				if(i != n/i) list.add((int)n/i);
+			}
+		}
+
+		System.out.print(0);
+		Iterator<Integer> x = list.iterator();
+		while(x.hasNext()){
+			System.out.print(" " + (x.next() - 1));
+		}
+		System.out.print(" " + (n - 1));
+	}
+}
