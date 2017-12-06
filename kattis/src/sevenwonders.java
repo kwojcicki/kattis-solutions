e6dc03fcbba57b4f21f3129e739b4f64b2d8fa7f
kwojcicki <kwojcicki@sympatico.ca>
2017-12-05 23:43:18 -0500

    added seven wonders krystian version

--- kattis/src/sevenwonders.java
+++ kattis/src/sevenwonders.java
@@ -3,6 +3,27 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class sevenwonders {
+	
+	// Krystian seven wonders
+//	public static void main(String[] args) throws IOException {
+//		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+//		char[] s = in.readLine().toCharArray();
+//		int t = 0;
+//		int c = 0;
+//		int g = 0;
+//		for(int i = 0; i < s.length;i++) {
+//			if(s[i] == 'T') {
+//				t++;
+//			} else if(s[i] == 'G') {
+//				g++;
+//			} else if(s[i] == 'C') {
+//				c++;
+//			}  
+//		}
+//		System.out.println((int)(Math.pow(t, 2) + Math.pow(c, 2) + Math.pow(g, 2) +
+//				7 * Math.min(Math.min(t, c), g)));
+//	}
+	
     public static void main(String[]args) throws IOException{
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         String input = in.readLine();
