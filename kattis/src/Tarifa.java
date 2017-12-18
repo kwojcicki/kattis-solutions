e7e7fa695bc5c63505b00c288e6c66855771c174
kwojcicki <kwojcicki@sympatico.ca>
2017-12-17 23:22:02 -0500

    added my version of tarifa

--- kattis/src/Tarifa.java
+++ kattis/src/Tarifa.java
@@ -2,6 +2,18 @@ import java.util.Scanner;
 
 public class Tarifa {
 
+	// Krystian Tarifa
+//	public static void main(String[] args) throws IOException {
+//		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+//		int n = Integer.parseInt(in.readLine());
+//		int s = Integer.parseInt(in.readLine());
+//		int sum = 0;
+//		for(int i = 0; i < s; i++) {
+//			sum += (n - Integer.parseInt(in.readLine()));
+//		}
+//		System.out.println(sum + n);
+//	}
+	
     public static void main(String[]args){
         Scanner s = new Scanner(System.in);
         int megaBytesPerMonth = s.nextInt();
