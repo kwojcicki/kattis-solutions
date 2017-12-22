1913eb0387aba207bcb2d1c7b5ed51490c7c4e2a
kwojcicki <kwojcicki@sympatico.ca>
2017-12-21 23:24:43 -0500

    added Pot

--- kattis/src/Pot.java
+++ kattis/src/Pot.java
@@ -2,15 +2,25 @@ import java.util.Scanner;
 
 public class Pot {
 
-    public static void main(String[]args){
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        int sum = 0;
-        for(int i = 0 ; i < n ; i ++){
-            int p = scan.nextInt();
-            sum += Math.pow(p/10, p%10);
-        }
-        System.out.println(sum);
-        scan.close();
-    }
+	// Krystian
+	//	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	//	int n = Integer.parseInt(in.readLine());
+	//	int sum = 0;
+	//	for(int i = 0; i < n; i++) {
+	//		int y = Integer.parseInt(in.readLine());
+	//		sum += Math.pow(y / 10, y%10);
+	//	}
+	//	System.out.println(sum);
+
+	public static void main(String[]args){
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int sum = 0;
+		for(int i = 0 ; i < n ; i ++){
+			int p = scan.nextInt();
+			sum += Math.pow(p/10, p%10);
+		}
+		System.out.println(sum);
+		scan.close();
+	}
 }
